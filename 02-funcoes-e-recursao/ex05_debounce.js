b0f0dd525d2484dd0fd0ebfcaf19208146859d44
ingridmoitinho <ingridmoitinho@gmail.com>
2025-06-09 21:00:42 -0300

    exercício 05: implementar debounce com função e delay como parâmetros

--- 02-funcoes-e-recursao/ex05_debounce.js
+++
@@ -1,4 +0,0 @@
-// 5. Debounce
-// Crie function debounce(fn, delay) que receba uma função fn e um delay
-// em ms, retornando uma nova função que só executa fn se não for
-// chamada novamente dentro do intervalo.
